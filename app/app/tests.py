135268aa484fc011c4007a2de16751890eeff5c3
Bytebud <sky.sravan@gmail.com>
2024-08-31 19:25:23 +0530

    Added Psycopg2 package.

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+"""
+    Sample test
+"""
+
+from django.test import SimpleTestCase
+from app.clac import add
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        resultset = add(10, 20)
+        self.assertEqual(resultset, 30)
+        
